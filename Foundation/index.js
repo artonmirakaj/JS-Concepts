095d607cd55724446829a06cb7837a0a4d0c9538
artonmirakaj <artonm26@gmail.com>
2023-11-01 12:37:36 -0400

    First Commit

---
+++ Foundation/index.js
@@ -0,0 +1,24 @@
+// javascript engine - runs JS code in the browser
+function jsengine(code) {
+  return code.split(/\s+/)
+}
+
+console.log(jsengine('var a = 5'));
+
+/*
+  Interpreters vs Compilers
+
+  There are 3 ways of translating code so that our compiters can understand
+
+  Interpreter
+  - read line by line
+
+  Compiler
+  - works ahead of time to create a translation of the code we've written
+  - takes a bit longer to start up
+  - they dont repeat the translation for each pass through (i.e. Looping)
+
+  JIT Compiler (Just In Time)
+  - combines interpreter and compiler
+  - is faster and optimizes code
+*/
